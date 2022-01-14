c7a8b37fbe9de2bda18e08cbd430064bb5911b4a
Alissa Luk <alissaluk1995@gmail.com>
2022-01-13 17:55:12 -0800

    implemented all answers query and working on question

--- database/qa/index.js
+++ database/qa/index.js
@@ -14,16 +14,15 @@ const allQuestions = async function (product_id, page, count, callback) {
   page = page || 1;
   count = count || 5;
   const queryArray = [product_id, page, count];
-  const queryString = 'SELECT * FROM questions WHERE product_id = 1 limit 5';
 
-  const queryString2 = "SELECT questions.*, json_agg(json_build_object(||'answers.answer_id'||, answers.*)) AS answers FROM questions LEFT JOIN answers ON answers.question_id = questions.question_id WHERE questions.product_id = 1 GROUP BY questions.question_id;"
+  const queryString2 = "SELECT questions.*, COALESCE(json_object_agg(answers.answer_id, json_build_object('id', answers.answer_id, 'body', answers.body, 'date', answers.date, 'answerer_name', answers.answerer_name, 'helpfulness', answers.helpfulness, 'photos', array_agg(photos.*))) FILTER (WHERE answers.answer_id IS NOT NULL), '{}') AS answers FROM questions LEFT JOIN answers ON answers.question_id = questions.question_id LEFT JOIN photos ON photos.answer_id = answers.answer_id WHERE questions.product_id = 1 GROUP BY questions.question_id;"
 
-  const queryString3 = "SELECT questions.*, jsonb_object_agg(answers.answer_id, answers.*) AS answers FROM questions LEFT JOIN answers ON answers.question_id = questions.question_id WHERE questions.product_id = 1 GROUP BY questions.question_id;"
+  const queryString3 = "SELECT questions.*, COALESCE(json_object_agg(answers.answer_id, json_build_object('id', answers.answer_id, 'body', answers.body, 'date', answers.date, 'answerer_name', answers.answerer_name, 'helpfulness', answers.helpfulness, 'photos', (json_build_object('id', photos.id, 'url', photos.url)))) FILTER (WHERE answers.answer_id IS NOT NULL), '{}') AS answers FROM questions LEFT JOIN answers ON answers.question_id = questions.question_id LEFT JOIN photos ON photos.answer_id = answers.answer_id WHERE questions.product_id = 1 GROUP BY questions.question_id;"
 
-  var questions;
-  var answers;
-  var photos;
-  await db.any(queryString2)
+  const queryString4 = "SELECT questions.* FROM questions LEFT JOIN ( "
+
+
+  await db.query(queryString2)
     .then((data) => {
       callback(null, data);
     })
@@ -31,21 +30,6 @@ const allQuestions = async function (product_id, page, count, callback) {
       callback(err);
     })
 
-//   db.task(t => {
-//     // execute a chain of queries against the task context, and return the result:
-//     return t.any('SELECT * FROM questions WHERE product_id = 1')
-//         .then(questions => {
-//             if(questions.length > 0) {
-//                 return t.any('SELECT * FROM answers WHERE question_id = $1', 1)
-//                     .then(logs => {
-//                       console.log(logs);
-//                       return {questions, logs};
-//                     })
-//             }
-//             console.log(questions);
-//             return {questions};
-//         });
-// })
     .then(data => {
       callback(null, data);
         // success, data = either {count} or {count, logs}
@@ -55,9 +39,43 @@ const allQuestions = async function (product_id, page, count, callback) {
     });
 }
 
+const allAnswers = function(question_id, page, count, callback) {
+  question_id = question_id || 1;
+  page = page || 1;
+  count = count || 5;
+
+  let queryString2 = "SELECT answers.answer_id, answers.body, answers.date, answers.answerer_name, answers.helpfulness, COALESCE(json_agg(json_build_object('id', photos.id, 'url', photos.url)) FILTER (WHERE photos.id IS NOT NULL), '[]') AS photos FROM answers LEFT JOIN photos ON photos.answer_id = answers.answer_id WHERE answers.question_id = $1 AND answers.reported != 1 GROUP BY answers.answer_id LIMIT $2;";
+
+  let queryArray = [question_id, count];
+
+  db.query(queryString2, queryArray)
+    .then((data) => {
+      data.forEach((answerobject) => {
+        answerobject.date = new Date(Number(answerobject.date));
+      })
+      let result = {
+        question: question_id,
+        page: page,
+        count: count,
+        results: data
+      }
+      callback(null, result);
+    })
+    .catch(err => {
+      callback(err);
+    });
+}
+
+const postQuestion = function(body, callback) {
+
+}
 
 console.log('Connected to the database');
 
 module.exports = {
-  pgp, db, allQuestions
+  pgp,
+  db,
+  allQuestions,
+  allAnswers,
+  postQuestion
 };
--- server/qa_server.js
+++ server/qa_server.js
@@ -1,16 +1,41 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const { db, allQuestions } = require('../database/qa/index.js');
+const { db, allQuestions, allAnswers } = require('../database/qa/index.js');
 
-app.get('/', (req, res) => {
-  allQuestions(1, 1, 5, (err, result) => {
+app.use(express.json());
+
+app.get('/qa/questions', (req, res) => {
+  console.log(req.query);
+  allQuestions(req.query.product_id, req.query.page, req.query.count, (err, result) => {
     if (err) {
       res.send(err);
     } else {
       res.send(result);
     }
   });
+});
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  allAnswers(req.params.question_id, req.query.page, req.query.count, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  })
+})
+
+app.post('/qa/questions/', (req, res) => {
+  console.log(req.params);
+  console.log(req.query);
+  console.log(req.body);
+  res.send('hello');
+})
+
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+  console.log(req.params);
+  console.log(req.query);
+  res.send('hello');
 })
 
 app.listen(port, () => {
