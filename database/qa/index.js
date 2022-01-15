b945a9874976e5a263f24f9605af7680a1387b26
Alissa Luk <alissaluk1995@gmail.com>
2022-01-14 19:31:27 -0800

    added endpoints with working queries

--- database/qa/index.js
+++ database/qa/index.js
@@ -15,28 +15,21 @@ const allQuestions = async function (product_id, page, count, callback) {
   count = count || 5;
   const queryArray = [product_id, page, count];
 
-  const queryString2 = "SELECT questions.*, COALESCE(json_object_agg(answers.answer_id, json_build_object('id', answers.answer_id, 'body', answers.body, 'date', answers.date, 'answerer_name', answers.answerer_name, 'helpfulness', answers.helpfulness, 'photos', array_agg(photos.*))) FILTER (WHERE answers.answer_id IS NOT NULL), '{}') AS answers FROM questions LEFT JOIN answers ON answers.question_id = questions.question_id LEFT JOIN photos ON photos.answer_id = answers.answer_id WHERE questions.product_id = 1 GROUP BY questions.question_id;"
-
-  const queryString3 = "SELECT questions.*, COALESCE(json_object_agg(answers.answer_id, json_build_object('id', answers.answer_id, 'body', answers.body, 'date', answers.date, 'answerer_name', answers.answerer_name, 'helpfulness', answers.helpfulness, 'photos', (json_build_object('id', photos.id, 'url', photos.url)))) FILTER (WHERE answers.answer_id IS NOT NULL), '{}') AS answers FROM questions LEFT JOIN answers ON answers.question_id = questions.question_id LEFT JOIN photos ON photos.answer_id = answers.answer_id WHERE questions.product_id = 1 GROUP BY questions.question_id;"
-
-  const queryString4 = "SELECT questions.* FROM questions LEFT JOIN ( "
+  const queryString3 = "SELECT questions.question_id, questions.question_body, to_timestamp(questions.question_date/1000) as question_date, questions.asker_name, questions.question_helpfulness, COALESCE(json_object_agg(answers.answer_id, json_build_object('id', answers.answer_id, 'body', answers.body, 'date', to_timestamp(answers.date/1000), 'answerer_name', answers.answerer_name, 'helpfulness', answers.helpfulness, 'photos',  json_build_array(json_strip_nulls(json_build_object('id', photos.id, 'url', photos.url))) )) FILTER (WHERE answers.answer_id IS NOT NULL), '{}') AS answers FROM questions LEFT JOIN answers ON answers.question_id = questions.question_id LEFT JOIN photos ON photos.answer_id = answers.answer_id WHERE questions.product_id = 1 AND questions.reported != 1 GROUP BY questions.question_id;"
 
 
-  await db.query(queryString2)
+  await db.query(queryString3)
     .then((data) => {
-      callback(null, data);
+      let result = {
+        product_id: product_id,
+        results: data
+      }
+      callback(null, result);
     })
     .catch(err => {
       callback(err);
     })
 
-    .then(data => {
-      callback(null, data);
-        // success, data = either {count} or {count, logs}
-    })
-    .catch(error => {
-        // failed
-    });
 }
 
 const allAnswers = function(question_id, page, count, callback) {
@@ -44,15 +37,13 @@ const allAnswers = function(question_id, page, count, callback) {
   page = page || 1;
   count = count || 5;
 
-  let queryString2 = "SELECT answers.answer_id, answers.body, answers.date, answers.answerer_name, answers.helpfulness, COALESCE(json_agg(json_build_object('id', photos.id, 'url', photos.url)) FILTER (WHERE photos.id IS NOT NULL), '[]') AS photos FROM answers LEFT JOIN photos ON photos.answer_id = answers.answer_id WHERE answers.question_id = $1 AND answers.reported != 1 GROUP BY answers.answer_id LIMIT $2;";
+  let queryString2 = "SELECT answers.answer_id, answers.body, to_timestamp(answers.date/1000) as date, answers.answerer_name, answers.helpfulness, COALESCE(json_agg(json_build_object('id', photos.id, 'url', photos.url)) FILTER (WHERE photos.id IS NOT NULL), '[]') AS photos FROM answers LEFT JOIN photos ON photos.answer_id = answers.answer_id WHERE answers.question_id = $1 AND answers.reported != 1 GROUP BY answers.answer_id LIMIT $2;";
 
   let queryArray = [question_id, count];
 
   db.query(queryString2, queryArray)
+
     .then((data) => {
-      data.forEach((answerobject) => {
-        answerobject.date = new Date(Number(answerobject.date));
-      })
       let result = {
         question: question_id,
         page: page,
@@ -66,8 +57,56 @@ const allAnswers = function(question_id, page, count, callback) {
     });
 }
 
-const postQuestion = function(body, callback) {
+const postQuestion = function(data, date, callback) {
+  let {body, name, email, product_id} = data;
+  let queryString = "INSERT INTO questions(product_id, question_body, question_date, asker_name, asker_email, reported, question_helpfulness) VALUES ($1, $2, $3, $4, $5, 0, 0);"
+  let queryArray = [product_id, body, date, name, email];
+  db.query(queryString, queryArray)
+    .then( result => callback(null))
+    .catch( err => callback(err));
+}
 
+const postAnswer = function(question_id, data, date, callback) {
+  let {body, name, email, photos} = data;
+  let queryString = "INSERT INTO answers(question_id, body, date, answerer_name, answerer_email, reported, helpfulness) VALUES ($1, $2, $3, $4, $5, 0, 0);"
+  let queryArray = [question_id, body, date, name, email];
+  db.query(queryString, queryArray)
+    .then( result => callback(null))
+    .catch( err => callback(err));
+
+  //PUT THE PICTURE
+}
+
+const qHelpfulness = function(question_id, callback) {
+  let queryString = "UPDATE questions SET question_helpfulness = question_helpfulness + 1 WHERE question_id = $1";
+  let queryArray = [question_id];
+  db.query(queryString, queryArray)
+    .then( result => callback(null))
+    .catch( err => callback(err));
+}
+
+const aHelpfulness = function(answer_id, callback) {
+  let queryString = "UPDATE answers SET helpfulness = helpfulness + 1 WHERE answer_id = $1";
+  let queryArray = [answer_id];
+  db.query(queryString, queryArray)
+    .then( result => callback(null))
+    .catch( err => callback(err));
+}
+
+const qReport = function(question_id, callback) {
+  let queryString = "UPDATE questions SET reported = reported + 1 WHERE question_id = $1";
+  let queryArray = [question_id];
+  db.query(queryString, queryArray)
+    .then( result => callback(null))
+    .catch( err => callback(err));
+}
+
+const aReport = function(answer_id, callback) {
+  let queryString = "UPDATE answers SET reported = reported + 1 WHERE answer_id = $1";
+  let queryArray = [answer_id];
+  db.query(queryString, queryArray)
+    .then( result => callback(null))
+    .catch( err => callback(err));
 }
 
 console.log('Connected to the database');
@@ -77,5 +116,10 @@ module.exports = {
   db,
   allQuestions,
   allAnswers,
-  postQuestion
+  postQuestion,
+  postAnswer,
+  qHelpfulness,
+  aHelpfulness,
+  qReport,
+  aReport
 };
--- server/qa_server.js
+++ server/qa_server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const { db, allQuestions, allAnswers } = require('../database/qa/index.js');
+const { db, allQuestions, allAnswers, postQuestion, postAnswer, qHelpfulness, aHelpfulness, qReport, aReport } = require('../database/qa/index.js');
 
 app.use(express.json());
 
@@ -26,16 +26,67 @@ app.get('/qa/questions/:question_id/answers', (req, res) => {
 })
 
 app.post('/qa/questions/', (req, res) => {
-  console.log(req.params);
-  console.log(req.query);
-  console.log(req.body);
-  res.send('hello');
+  const date = Date.now();
+  postQuestion(req.body, date, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send('Created');
+    }
+  })
 })
 
 app.post('/qa/questions/:question_id/answers', (req, res) => {
   console.log(req.params);
-  console.log(req.query);
-  res.send('hello');
+  console.log(req.body);
+  const date = Date.now();
+  postAnswer(req.params.question_id, req.body, date, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send('Created');
+    }
+  })
+})
+
+app.put('/qa/questions/:question_id/helpful', (req,res) => {
+  qHelpfulness(req.params.question_id, (err, result) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.status(204).send();
+    }
+  });
+})
+
+app.put('/qa/questions/:question_id/report', (req,res) => {
+  qReport(req.params.question_id, (err, result) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.status(204).send();
+    }
+  });
+})
+
+app.put('/qa/answers/:answer_id/helpful', (req,res) => {
+  aHelpfulness(req.params.answer_id, (err, result) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.status(204).send();
+    }
+  });
+})
+
+app.put('/qa/answers/:answer_id/report', (req,res) => {
+  aReport(req.params.answer_id, (err, result) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.status(204).send();
+    }
+  });
 })
 
 app.listen(port, () => {
